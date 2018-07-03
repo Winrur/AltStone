909671fffae31890c08a1592a3282a6caa38c049
Winrur <35635213+Winrur@users.noreply.github.com>
2018-07-04 02:12:26 +0300

    Add files via upload

--- join-game-button.js
+++ join-game-button.js
@@ -7,14 +7,12 @@ AFRAME.registerComponent("join-leave-game", {
         var player2 = [];
         var player1joined = false;
         var player2joined = false;
-        fbRef.doc("States").set({
-          p1joined: false,
-          p2joined: false
-        })
         fbRef.doc("States").onSnapshot(function(doc){
           var data = doc.data();
-          player1joined = data.p1joined;
-          player2joined = data.p2joined;
+        fbRef.doc("States").set({
+          p1joined: player1joined,
+          p2joined: player2joined
+        })
           console.log(player1joined);
         })
         fbRef.doc("Player 1").delete();
@@ -27,6 +25,7 @@ AFRAME.registerComponent("join-leave-game", {
                     userId = user.userId;
                     displayName = user.displayName;
                     el.setAttribute("color", "red");
+                    player1joined = true;
                     console.log(displayName + " joined the game.");
                     console.log(player1joined);
                     console.log(player2joined);
@@ -41,7 +40,7 @@ AFRAME.registerComponent("join-leave-game", {
                           p1joined: true
                         })
                     }
-                    if (player1joined == true) {
+                    if (player1joined == true && player2joined == false) {
                         player2.push(displayName);
                         player2.push(userId);
                         fbRef.doc("Player 2").set({
@@ -51,6 +50,7 @@ AFRAME.registerComponent("join-leave-game", {
                         fbRef.doc("States").update({
                           p2joined: true
                         })
+                        player2joined = true;
                     }
                     if (el.getAttribute("color") == "red") {
                         el.object3D.addEventListener("cursordown", function() {
