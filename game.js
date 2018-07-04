5e076a7f53691b9fd349d0f04ed83917b229cda5
Winrur <35635213+Winrur@users.noreply.github.com>
2018-07-04 05:38:14 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,63 @@
+  var turnCounter = 0
+  var cardArr = [];
+  var cardData;
+  for(var i = 53; i <= 220; i++){
+    var currentCard = cardList.Basic[i]
+    console.log(i);
+    cardArr[i] = new Card( currentCard.name,
+                           currentCard.type,
+                           currentCard.playerClass,
+                           currentCard.cost,
+                           currentCard.attack,
+                           currentCard.health,
+                           currentCard.dbfId,
+                           currentCard.imgGold,
+                           "https://raw.githubusercontent.com/schmich/hearthstone-card-images/master/rel/" + currentCard.dbfId + ".png");
+  };
+  console.log(cardArr);
+  var druidDeck = [ cardArr[57], //Innervate
+                    cardArr[57], //Innervate
+                    cardArr[68], //Claw
+                    cardArr[68], //Claw
+                    cardArr[126], //Mark of the Wild
+                    cardArr[126], //Mark of the Wild
+                    cardArr[141], //Wild Growth
+                    cardArr[141], //Wild Growth
+                    cardArr[187], //Swipe
+                    cardArr[187], //Swipe
+                    cardArr[211], //Starfire
+                    cardArr[211], //Starfire
+                    cardArr[219], //Ironbark Protector
+                    cardArr[219], //Ironbark Protector
+                    cardArr[106], //Acidic Swamp Ooze
+                    cardArr[106], //Acidic Swamp Ooze
+                    cardArr[109], //Bloodfen Raptor
+                    cardArr[109], //Bloodfen Raptor
+                    cardArr[165], //Shattered Sun Cleric 
+                    cardArr[171], //Chillwind Yeti
+                    cardArr[171], //Chillwind Yeti
+                    cardArr[175], //Gnomish Inventor
+                    cardArr[175], //Gnomish Inventor
+                    cardArr[185],//Sen'jin Shieldmasta
+                    cardArr[185],//Sen'jin Shieldmasta
+                    cardArr[196],//Darkscale Healer
+                    cardArr[206],//Boulderfist Ogre
+                    cardArr[186], //Stormwind Champion
+                    cardArr[186] //Stormwind Champion
+                  ];
+  function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+}
+function setupGame(){
+  turnCounter = 0;
+  shuffle(druidDeck);
+  fbRef.doc("Cards").set({
+
+  })
+
+};
+setupGame();
--- join-game-button.js
+++ join-game-button.js
@@ -1,72 +1,100 @@
-AFRAME.registerComponent("join-leave-game", {
+var p1join = false;
+var p1JoinText = document.getElementById("join-game-p1-text")
+var p2join= false;
+var p2JoinText = document.getElementById("join-game-p2-text")
+AFRAME.registerComponent("join-game-p1", {
     schema: {},
     init: function() {
         var el = this.el;
-        var playerText = document.getElementById("player-text");
-        var player1 = [];
-        var player2 = [];
-        var player1joined = false;
-        var player2joined = false;
-        fbRef.doc("States").onSnapshot(function(doc){
-          var data = doc.data();
-        fbRef.doc("States").set({
-          p1joined: player1joined,
-          p2joined: player2joined
-        })
-          console.log(player1joined);
-        })
+        var player1 = [];;
+        var p1AboveText = document.getElementById("p1-above-text");
+        var userId;
+        var displayName;
         fbRef.doc("Player 1").delete();
-        fbRef.doc("Player 2").delete();
         el.object3D.addEventListener("cursordown", function() {
-            var userId;
-            var displayName;
-            if (player1joined == false || player2joined == false) {
+            if (p1join == false && p2join == false) {
                 altspace.getUser().then(function(user) {
                     userId = user.userId;
                     displayName = user.displayName;
+                    p1AboveText.setAttribute("visible", true);
+                    p1AboveText.setAttribute("n-skeleton-parent", {part: "head", userId: userId});
+                    p1join = true;
                     el.setAttribute("color", "red");
-                    player1joined = true;
-                    console.log(displayName + " joined the game.");
-                    console.log(player1joined);
-                    console.log(player2joined);
-                    if (player1.length == 0 && el.getAttribute("color") == "red") {
-                        player1.push(displayName);
-                        player1.push(userId);
-                        fbRef.doc("Player 1").set({
-                            displayName: player1[0],
-                            userId: player1[1],
-                        });
-                    }
-                    if (player1joined == true && player2joined == false) {
-                        player2.push(displayName);
-                        player2.push(userId);
-                        fbRef.doc("Player 2").set({
-                            displayName: player2[0],
-                            userId: player2[1]
-                        });
-                        player2joined = true;
-                    }
-                    if (el.getAttribute("color") == "red") {
-                        el.object3D.addEventListener("cursordown", function() {
-                            if (player1joined == true) {
-                                player1 = [];
-                                fbRef.doc("Player 1").delete();
-                                el.setAttribute("color", "green");
-                                console.log(displayName + " left the game.");
-                                console.log(player1joined);
-                            }
-                            if (player2joined == true) {
-                                player2 = [];
-                                fbRef.doc("Player 2").delete();
-                                el.setAttribute("color", "green");
-                                console.log(displayName + " left the game.");
-                                console.log(player2joined);
-                            }
-
-                        });
-                    }
-                });
+                    p1JoinText.setAttribute("n-text", "text", "Waiting for opponent...");
+                    console.log(displayName + " joined the game as P1.");
+                    player1.push(displayName);
+                    player1.push(userId);
+                    fbRef.doc("Player 1").set({
+                        displayName: player1[0],
+                        userId: player1[1],
+                    });
+                })
+            } else if (el.getAttribute("color") == "red") {
+              p1AboveText.setAttribute("visible", false);
+              p1join = false;
+              el.setAttribute("color", "green");
+              p1JoinText.setAttribute("n-text", "text", "Join game! :)");
+              console.log(displayName + " left the game.");
+              player1 = [];
+              fbRef.doc("Player 1").delete();
             }
-        });
+        })
     }
+
 });
+
+AFRAME.registerComponent("join-game-p2", {
+    schema: {},
+    init: function() {
+        var el = this.el;
+        var player2 = [];
+        var p2AboveText = document.getElementById("p2-above-text");
+        var userId;
+        var displayName;
+        fbRef.doc("Player 2").delete();
+        el.object3D.addEventListener("cursordown", function() {
+            if (p2join == false && p1join == false) {
+                altspace.getUser().then(function(user) {
+                    userId = user.userId;
+                    displayName = user.displayName;
+                    p2AboveText.setAttribute("visible", true);
+                    p2AboveText.setAttribute("n-skeleton-parent", {part: "head", userId: userId});
+                    p2join = true;
+                    el.setAttribute("color", "red");
+                    p2JoinText.setAttribute("n-text", "text", "Waiting for opponent...");
+                    console.log(displayName + " joined the game as P2.");
+                    player2.push(displayName);
+                    player2.push(userId);
+                    fbRef.doc("Player 2").set({
+                        displayName: player2[0],
+                        userId: player2[1],
+                    });
+                })
+            } else if (el.getAttribute("color") == "red") {
+              p2AboveText.setAttribute("visible", false);
+              p2join = false;
+              el.setAttribute("color", "green");
+              p2JoinText.setAttribute("n-text", "text", "Join game! :)");
+              console.log(displayName + " left the game.");
+              player2 = [];
+              fbRef.doc("Player 2").delete();
+            }
+        })
+    }
+
+});
+AFRAME.registerComponent("game-start", {
+  schema: {},
+  init: function(){
+    var p1JoinBox = document.getElementById("p1-join-box");
+    var p2JoinBox = document.getElementById("p2-join-box");
+    if (p1join == true && p2join == true) {
+      function startGame(){
+        p1JoinBox.setAttribute("visible", false);
+        p2JoinBox.setAttribute("visible", false);
+        p1JoinText.setAttribute("visible", false);
+        p2JoinText.setAttribute("visible", false);
+      }
+    }
+  }
+})
