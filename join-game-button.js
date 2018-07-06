7dd9d25caf58d975ada172caa45f1fc42a3b2594
Winrur <35635213+Winrur@users.noreply.github.com>
2018-07-06 07:03:03 +0300

    Add files via upload

--- join-game-button.js
+++ join-game-button.js
@@ -1,114 +1,106 @@
-var p1join = false;
-var p1color = "green";
-var p1JoinText = document.getElementById("join-game-p1-text");
-var p2join= false;
-var p2color = "green";
-var p2JoinText = document.getElementById("join-game-p2-text");
-fbRef.doc("States").onSnapshot(function(doc){
-  var data = doc.data();
-  p1color = data.p1color;
-  p2color = data.p2color;
-})
+var p1Joined = false;
+var p2Joined = false;
 AFRAME.registerComponent("join-game-p1", {
     schema: {},
     init: function() {
         var el = this.el;
-        var player1 = [];;
-        var p1AboveText = document.getElementById("p1-above-text");
+        var p1JoinText = document.getElementById("join-game-p1-text");
         var userId;
         var displayName;
-        fbRef.doc("Player 1").delete();
         fbRef.doc("States").update({
-          p1color: "green"
+            p1joined: p1Joined
         })
-        el.object3D.addEventListener("cursordown", function() {
-            if (p1join == false && p2join == false) {
-                altspace.getUser().then(function(user) {
-                    userId = user.userId;
-                    displayName = user.displayName;
-                    p1join = true;
-                    p1AboveText.setAttribute("visible", true);
-                    p1AboveText.setAttribute("n-skeleton-parent", {part: "head", userId: userId});
-                    fbRef.doc("States").update({
-                      p1color: "red"
-                    })
-                    el.setAttribute("color", p1color);
-                    //p1JoinText.setAttribute("n-text", "text", "Waiting for opponent...");
-                    console.log(displayName + " joined the game as P1.");
-                    player1.push(displayName);
-                    player1.push(userId);
-                    fbRef.doc("Player 1").set({
-                        displayName: player1[0],
-                        userId: player1[1],
-                    });
-                })
-            } else if (el.getAttribute("color") == "red") {
-              p1AboveText.setAttribute("visible", false);
-              p1join = false;
-              el.setAttribute("color", "green");
-              //p1JoinText.setAttribute("n-text", "text", "Join game! :)");
-              console.log(displayName + " left the game.");
-              player1 = [];
-              fbRef.doc("Player 1").delete();
-            }
+        fbRef.doc("States").onSnapshot(function(doc){
+            var data = doc.data();
+            if (data.p1joined == true) {
+                el.setAttribute("material", "opacity", 0);
+                p1JoinText.setAttribute("visible", false);
+        }
+            if (data.p1joined == false) {
+                el.setAttribute("material", "opacity", 1);
+                p1JoinText.setAttribute("visible", true);
+        }
         })
+        el.object3D.addEventListener("triggerenter", function() {
+            if (el.getAttribute("id") == "p1-join-box") {
+            p1Joined = true;
+            fbRef.doc("States").update({
+                        p1joined: p1Joined
+            })
+            
+          }
+
+    })
+        el.object3D.addEventListener("triggerexit", function() {
+            if (el.getAttribute("id") == "p1-join-box") {
+            p1Joined = false;
+            fbRef.doc("States").update({
+                        p1joined: p1Joined
+            })
+            
+          }
+
+    })
     }
 
 });
-
 AFRAME.registerComponent("join-game-p2", {
     schema: {},
     init: function() {
         var el = this.el;
-        var player2 = [];
-        var p2AboveText = document.getElementById("p2-above-text");
+        var p2JoinText = document.getElementById("join-game-p2-text");
         var userId;
         var displayName;
-        fbRef.doc("Player 2").delete();
-        el.object3D.addEventListener("cursordown", function() {
-            if (p2join == false && p1join == false) {
-                altspace.getUser().then(function(user) {
-                    userId = user.userId;
-                    displayName = user.displayName;
-                    p2AboveText.setAttribute("visible", true);
-                    p2AboveText.setAttribute("n-skeleton-parent", {part: "head", userId: userId});
-                    p2join = true;
-                    el.setAttribute("color", "red");
-                    //p2JoinText.setAttribute("n-text", "text", "Waiting for opponent...");
-                    console.log(displayName + " joined the game as P2.");
-                    player2.push(displayName);
-                    player2.push(userId);
-                    fbRef.doc("Player 2").set({
-                        displayName: player2[0],
-                        userId: player2[1],
-                    });
-                })
-            } else if (el.getAttribute("color") == "red") {
-              p2AboveText.setAttribute("visible", false);
-              p2join = false;
-              el.setAttribute("color", "green");
-              //p2JoinText.setAttribute("n-text", "text", "Join game! :)");
-              console.log(displayName + " left the game.");
-              player2 = [];
-              fbRef.doc("Player 2").delete();
+        fbRef.doc("States").update({
+            p2joined: p2Joined
+        })
+        fbRef.doc("States").onSnapshot(function(doc){
+            var data = doc.data();
+            if (data.p2joined == true) {
+                el.setAttribute("material", "opacity", 0);
+                p2JoinText.setAttribute("visible", false);
+        }
+            if (data.p2joined == false) {
+                el.setAttribute("material", "opacity", 1);
+                p2JoinText.setAttribute("visible", true);
+        }
+        })
+        el.object3D.addEventListener("triggerenter", function() {
+            if (el.getAttribute("id") == "p2-join-box") {
+            p2Joined = true;
+            fbRef.doc("States").update({
+                        p2joined: p2Joined
+            })
+            
+          }
+
+    })
+        el.object3D.addEventListener("triggerexit", function() {
+            if (el.getAttribute("id") == "p2-join-box") {
+            p2Joined = false;
+            fbRef.doc("States").update({
+                        p2joined: p2Joined
+            })
+            
+          }
+
+    })
+    }
+
+});
+AFRAME.registerComponent("start-game", {
+    schema: {},
+    init: function() {
+        var el = this.el;
+        var p1JoinBox = document.getElementById("p1-join-box");
+        var p2JoinBox = document.getElementById("p2-join-box");
+        fbRef.doc("States").get().then(function(doc){
+            var data = doc.data();
+            if (data.p1joined == true && data.p2joined == true) {
+                p1JoinBox.setAttribute("visible", false);
+                p2JoinBox.setAttribute("visible", false);
             }
         })
     }
 
 });
-AFRAME.registerComponent("game-start", {
-  schema: {},
-  init: function(){
-    var p1JoinBox = document.getElementById("p1-join-box");
-    var p2JoinBox = document.getElementById("p2-join-box");
-    if (p1join == true && p2join == true) {
-      function startGame(){
-        p1JoinBox.setAttribute("visible", false);
-        p2JoinBox.setAttribute("visible", false);
-        p1JoinText.setAttribute("visible", false);
-        p2JoinText.setAttribute("visible", false);
-      }
-    }
-    startGame();
-  }
-})
