3b7c6a792e09143931f5a758ea52b721d15c245f
Winrur <35635213+Winrur@users.noreply.github.com>
2018-07-06 11:43:54 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -93,7 +93,7 @@ function setupGame(){
   turnCounter = 0;
   shuffle(p1Deck);
   shuffle(p2Deck);
-  for (let i = 1; i > p1Deck.length; i++) {
+  for (let i = 1; i < p1Deck.length; i++) {
   fbRef.doc("Player 1 Cards").update({
       i: p1Deck[i]
   })
