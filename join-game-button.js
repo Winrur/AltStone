4e56f2f0558c778e2294180424178ab79ae2629d
Winrur <35635213+Winrur@users.noreply.github.com>
2018-07-06 07:32:12 +0300

    Add files via upload

--- join-game-button.js
+++ join-game-button.js
@@ -1,5 +1,6 @@
 var p1Joined = false;
 var p2Joined = false;
+var gameStarted = false;
 AFRAME.registerComponent("join-game-p1", {
     schema: {},
     init: function() {
@@ -12,11 +13,11 @@ AFRAME.registerComponent("join-game-p1", {
         })
         fbRef.doc("States").onSnapshot(function(doc){
             var data = doc.data();
-            if (data.p1joined == true) {
+            if (data.p1joined == true && gameStarted == false) {
                 el.setAttribute("material", "opacity", 0);
                 p1JoinText.setAttribute("visible", false);
         }
-            if (data.p1joined == false) {
+            if (data.p1joined == false && gameStarted == false) {
                 el.setAttribute("material", "opacity", 1);
                 p1JoinText.setAttribute("visible", true);
         }
@@ -56,11 +57,11 @@ AFRAME.registerComponent("join-game-p2", {
         })
         fbRef.doc("States").onSnapshot(function(doc){
             var data = doc.data();
-            if (data.p2joined == true) {
+            if (data.p2joined == true && gameStarted == false) {
                 el.setAttribute("material", "opacity", 0);
                 p2JoinText.setAttribute("visible", false);
         }
-            if (data.p2joined == false) {
+            if (data.p2joined == false && gameStarted == false) {
                 el.setAttribute("material", "opacity", 1);
                 p2JoinText.setAttribute("visible", true);
         }
@@ -94,11 +95,16 @@ AFRAME.registerComponent("start-game", {
         var el = this.el;
         var p1JoinBox = document.getElementById("p1-join-box");
         var p2JoinBox = document.getElementById("p2-join-box");
+        fbRef.doc("States").update({
+            gamestarted: gameStarted
+        })
         fbRef.doc("States").get().then(function(doc){
             var data = doc.data();
             if (data.p1joined == true && data.p2joined == true) {
-                p1JoinBox.setAttribute("visible", false);
-                p2JoinBox.setAttribute("visible", false);
+                gameStarted = true;
+        fbRef.doc("States").update({
+            gamestarted: gameStarted
+        })
             }
         })
     }
